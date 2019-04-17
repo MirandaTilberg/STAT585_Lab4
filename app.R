3935f9cdd43e9ea98d9d58059cd9c9f628d2e8c8
qiaoyang00 <qiaoyang00@126.com>
2019-04-17 17:38:14 -0500

    Add leaflet map in Shiny.

--- app.R
+++ app.R
@@ -7,13 +7,13 @@ ui <- fluidPage(
   #choice is the option for the input
   #multiple is whether input could be multiple choice
   selectizeInput(inputId = "code", label = "XXX", 
-                 choices=XXXX, 
+                 choices=1:5, 
                  selected=NULL,
                  multiple=T),
   #check box menu
   #we can add more kinds of input menu if we need
   checkboxGroupInput(inputId = "feature", label = "XXX", 
-                     choices=c(XXXX), 
+                     choices=c(1:5), 
                      selected=NULL),
   #set tab
   mainPanel(
@@ -21,20 +21,35 @@ ui <- fluidPage(
       #temporal tab
       #plot name in the plotOutput
       tabPanel("temporal", plotOutput("plot_temporal")),
-      tabPanel("spatial", plotOutput("plot_saptial"))
+      tabPanel("spatial", leaflet::leafletOutput("map"))
     )
   )
 ) 
 
 server <- function(input, output) {
+  raw_df <- "https://data.iowa.gov/resource/m3tr-qhgy.json?county=Story" %>%
+    jsonlite::read_json() %>%
+    purrr::map_df(~ dplyr::as_tibble(as.list(unlist(.x, use.names = T))))
+  
+  df <- raw_df %>% mutate(date = ymd_hms(date),
+                          longitude = as.numeric(store_location.coordinates1),
+                          latitude = as.numeric(store_location.coordinates2)) %>%
+    filter(!is.na(latitude)) %>%
+    filter(!is.na(longitude))
+  
   output$plot_temporal <- renderPlot({
     #plot function
-    plot_temporal_function()
+    # plot_temporal_function()
   })
   output$plot_saptial <- renderPlot({
     #plot function
-    plot_temporal_function()
+    # plot_temporal_function()
     }
+  )
+  output$map <- leaflet::renderLeaflet({
+    leaflet::leaflet(data = df) %>% leaflet::addTiles() %>%
+    leaflet::addCircleMarkers(~longitude, ~latitude, popup = ~name, label = ~name,
+                              clusterOptions = leaflet::markerClusterOptions())
   })
 }
 
