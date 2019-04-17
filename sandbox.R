1ae5ab3bbb002c7c4525c71f82d7fabea5e3f45f
Miranda Tilton <miranda.tilton@gmail.com>
2019-04-17 17:11:20 -0500

    Try to use leaflet

--- sandbox.R
+++ sandbox.R
@@ -1,2 +1,16 @@
+raw_df <- purrr::map_df(jsonlite::read_json("https://data.iowa.gov/resource/m3tr-qhgy.json?county=Story"), ~ dplyr::as_tibble(as.list(unlist(.x, use.names = T))))
+names(raw_df)
+
+
+library(tidyverse)
 library(magrittr)
-df <- jsonlite::read_json("https://data.iowa.gov/resource/m3tr-qhgy.json?county=Story") %>% purrr::map_df(~ dplyr::as_tibble(as.list(unlist(.x, use.names = T))))
+library(lubridate)
+df <- raw_df %>% mutate(date = ymd_hms(date),
+                        longitude = as.numeric(store_location.coordinates1),
+                        latitude = as.numeric(store_location.coordinates2),)
+
+
+zipmap <- leaflet::leaflet(data = df) %>% leaflet::addTiles() %>%
+  leaflet::addCircleMarkers(~as.numeric(store_location.coordinates1), 
+                            ~as.numeric(store_location.coordinates2), 
+                            popup = ~name, label = ~name)
